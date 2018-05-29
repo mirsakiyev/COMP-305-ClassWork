e5b5c95820474364a3d6a989fa7f93b14435fb99
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-05-29 12:15:37 -0400

    Added May29 class work

---
+++ ClassWork_May29_AslanMirsakiyev/Assets/CameraController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public enum CameraMode
+    {
+        StayPut = 0,
+        FollowPlayer = 1,
+        HandHeldCamera = 2,
+    }
+
+    public CameraMode cameraMode = CameraMode.StayPut;
+    public float HandHeldRadius = 5f;
+    public float waitForHendHeldFollow = 0.1f; //sec
+    public string ____________________________;
+    private GameObject player;
+    private Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start () 
+    {
+        player=GameObject.FindGameObjectWithTag("Player");
+        cameraOffset = this.gameObject.transform.position - player.transform.position;
+	}
+
+    IEnumerable HandHeldFollow()
+    {
+        yield return new WaitForSeconds(1f);
+	    Vector3 newPosCandidate = this.transform.position = player.transform.position;
+		Vector3 rnd = UnityEngine.Random.insideUnitCircle * HandHeldRadius;
+		this.transform.position = newPosCandidate - new Vector3(rnd.x, rnd.y, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		switch (cameraMode)
+        {
+            case CameraMode.StayPut:
+                this.transform.position = player.transform.position + cameraOffset;
+                break;
+                 
+            case CameraMode.FollowPlayer:
+                StartCoroutine("HandHeldFollow");
+                break;
+
+            case CameraMode.HandHeldCamera:
+                break;
+
+            default:
+                break;
+        }
+	}
+}
---
+++ ClassWork_May29_AslanMirsakiyev/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+        print("Input.mousePosition:" + Input.mousePosition);
+        float mx = Input.GetAxis("Horizontal");
+        float mxr = Input.GetAxisRaw("Horizontal");
+        float my = Input.GetAxis("Vertical");
+        float myr = Input.GetAxisRaw("Vertical");
+
+        print("mx: "+mx+ "mxr: "+mxr);
+        print("my: "+my+ "myr: "+myr);
+
+        Vector3 dir = new Vector3(mx, my,0);
+        dir = dir.normalized;
+
+        transform.position += dir*speed*Time.deltaTime;
+
+	}
+}
