42d16a2e673d0fb7dbad36273453e05a53600078
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-05-31 16:19:51 -0400

    Added May31 class work

---
+++ ClassWork_May31_AslanMirsakiyev/Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Dictionary<string, GameObject> pieces = new Dictionary<string, GameObject>();
+    private string[] keys={"O","I","J"};
+    private GameObject currentGO;
+
+
+    public void NextPiece()
+    {
+		currentKey = keys[Random.Range(0, keys.Length)];
+		currentGO = pieces[currentKey];
+		Instantiate(currentGO, new Vector3(0, 12, 0), Quaternion.identity);
+
+    }
+    private string currentKey;
+
+    public GameObject tetrisOPrefab;
+    // [][]
+    // [][]
+
+    public GameObject tetrisIPrefab;
+	// []
+    // []
+    // []
+    // []
+
+	public GameObject tetrisJPrefab;
+    //   []
+    //   []
+    //   []
+    // [][]
+
+
+	// Use this for initialization
+	void Start () {
+		pieces["O"] = tetrisOPrefab;
+        pieces["I"] = tetrisIPrefab;
+        pieces["J"] = tetrisJPrefab;
+        NextPiece();
+     
+
+        //pieces["L"] = tetrisLPrefab;
+        //pieces["S"] = tetrisSPrefab;
+        //pieces["Z"] = tetrisZPrefab;
+        //pieces["T"] = tetrisTPrefab;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //yield return new WaitForSeconds(5);
+	}
+}
---
+++ ClassWork_May31_AslanMirsakiyev/Assets/Scripts/PieceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceController : MonoBehaviour {
+    public float speed=2f;
+    public GameController gameController;
+
+    // Use this for initialization
+    void Start () {
+        gameController = FindObjectOfType<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position -= new Vector3(0, speed * Time.deltaTime);  
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (other.gameObject.tag) ;
+        gameController.NextPiece();
+    }
+}
