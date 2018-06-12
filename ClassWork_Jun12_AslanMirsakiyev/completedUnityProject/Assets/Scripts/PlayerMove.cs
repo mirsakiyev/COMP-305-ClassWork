4f763529de6a8626748781400e7c420c1838e428
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-12 12:29:24 -0400

    Added Jun12 class work

---
+++ ClassWork_Jun12_AslanMirsakiyev/completedUnityProject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// ClassWork_Jun12_AslanMirsakiyev
+// id: 300850326
+
+public class PlayerMove : MonoBehaviour {
+	public float speed = 10;
+	private Rigidbody2D rigidBody2D;
+
+    private float deltaTime = 1;
+	
+	void Awake(){
+		rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate(){
+		float xMove = Input.GetAxis("Horizontal");
+		float yMove = Input.GetAxis("Vertical");
+		
+		float xSpeed = xMove * speed;
+		float ySpeed = yMove * speed;
+		
+		Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
+		
+		rigidBody2D.velocity = newVelocity;
+	}
+}
