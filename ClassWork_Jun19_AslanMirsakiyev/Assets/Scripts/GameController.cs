d88f35ab31c344b149e5ec483f8ac3cda05881ae
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2018-06-19 12:27:12 -0400

    Added Jun19 class work

---
+++ ClassWork_Jun19_AslanMirsakiyev/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    
+
+    public GameObject[] prefabs = new GameObject[7];
+
+    public float prefabDelay = 20;
+    public float timeElapsedToNextPiece = 0; // seconds
+    public Transform respawn;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeElapsedToNextPiece += Time.deltaTime;
+
+        if (timeElapsedToNextPiece > prefabDelay)
+        {
+
+            int idx = Random.Range(0, prefabs.Length);
+            GameObject newPrefab = prefabs[idx];
+            Instantiate(newPrefab, respawn);
+        }
+
+    }
+}
